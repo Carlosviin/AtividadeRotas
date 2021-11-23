6ffdf0ccfbaf7949369a0bec93e6b45c9f26d4cf
Carlosviin <carlosvin26@gmail.com>
2021-11-23 16:59:41 -0300

    Atividade Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'Home',
+    component:HomeComponent
+  },
+  {
+    path:'About',
+    component:AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
